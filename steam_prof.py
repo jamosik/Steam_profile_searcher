927e6e7eb14cf77df2817ad0df882468a4b7f2d0
Nikita <94798537+jamosik@users.noreply.github.com>
2025-07-27 18:32:27 +0200

    Add files via upload

--- steam_prof.py
+++ steam_prof.py
@@ -1,4 +1,6 @@
+import requests
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QPixmap
 from steam_web_api import Steam
 import os
 import sys
@@ -8,47 +10,60 @@ class UI(QtWidgets.QMainWindow):
         super(UI,self).__init__()
         uic.loadUi('Main_Steam.ui',self)
         self.show()
-        
+
         self.Steamid_Submit_Btn.clicked.connect(self.SteamIdSubmit)
-        
+
         self.AppStart()
-        
+
         self.SteamKey = os.environ.get("STEAM_API_KEY")
         self.steam = Steam(self.SteamKey)
-        
-        
-        
-    
+
+
+
+
+
     def SteamIdSubmit(self):
-        self.steamid = self.Steamid_Input.text() 
+        self.steamid = self.Steamid_Input.text().strip()
         #my steam id: 76561198140493349
-        
-               
-        if  self.steamid.strip():
-        
+
+
+        if  self.steamid and len(self.steamid) == 17:
+
             try:
-                self.steam.users.get_user_details(self.steamid)
+                res = self.steam.users.get_user_details(self.steamid)["player"]
                 self.SteamIdErr.setVisible(False)
                 self.Result.setVisible(True)
-                self.ProfName.setText("PLaceholder")
-            
-            except:
+                self.ProfName.setText(res["personaname"])
+                self.ProfImage.setPixmap(self.GetImage(res["avatarfull"]))
+
+
+            except Exception as e:
+                print(e)
                 self.SteamIdErr.setVisible(True)
                 self.Result.setVisible(False)
-            
+
         else:
             self.SteamIdErr.setVisible(True)
             self.Result.setVisible(False)
-            
-    
-            
-        
+
+
+
+
     def AppStart(self):
         self.Result.setVisible(False)
         self.SteamIdErr.setVisible(False)
 
+    def GetImage(self, URL):
+        response = requests.get(URL)
+        pixmap = QPixmap()
+        pixmap.loadFromData(response.content)
+        return pixmap
+
+
 app = QtWidgets.QApplication(sys.argv)
 window = UI()
 app.exec_()
+
+
 
 
